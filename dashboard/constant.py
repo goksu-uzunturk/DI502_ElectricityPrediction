f06ca5e8acaec477b3a6d4f937ee493be40957e5
ezgitncy <70578764+ezgitncy@users.noreply.github.com>
2023-11-25 13:33:31 +0300

    Add files via upload

---
+++ dashboard/constant.py
@@ -0,0 +1,39 @@
+list_of_models = ['Linear_Regression','Decision Tree', 'Moving Average', 'XGBoost', 'ARIMA']
+
+NYISO_DESCRIPTION = '''
+    "The NYISO is the New York Independent System Operator — the organization
+    responsible for managing New York’s electric grid and its competitive
+    wholesale electric marketplace." For more information, visit
+    https://www.nyiso.com/.
+'''
+LR_MODEL_DESCRIPTION = '''
+    The Linear Regression forecasting model was trained on historical meter readings, weather, and building data
+    from 2016-2017. Temperature readings are from site_id - 1 and site_id - 6.
+'''
+Linear_MAE = 0.69
+
+LR_MODEL_DESCRIPTION = '''
+    The Linear Regression forecasting model was trained on historical meter readings, weather, and building data
+    from 2016-2017. Temperature readings are from site_id - 1 and site_id - 6.
+'''
+Linear_MAE = 0.69
+
+DecisionTree_MODEL_DESCRIPTION = '''
+    The Decision Tree forecasting model was trained on historical meter readings, weather, and building data
+    from 2016-2017. Temperature readings are from site_id - 1 and site_id - 6.
+'''
+DTR_MAE = 0.38
+
+XGBoost_MODEL_DESCRIPTION = '''
+    The XG Boost forecasting model was trained on historical meter readings, weather, and building data
+    from 2016-2017. Temperature readings are from site_id - 1 and site_id - 6.
+'''
+XGBoost1_MAE = 0.29
+XGBoost1_MAE = 0.35
+XGBoost1_MAE = 0.38
+XGBoost1_MAE = 0.34
+XGBoost1_MAE = 0.42
+
+
+
+
